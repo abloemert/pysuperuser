7f6e03bb0b9cfb8a484dc115ca75a212b31d2241
abloemert <abloemert@gmail.com>
2022-09-21 15:02:30 +0200

    Add dependency to pywin32 on Windows platform

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     name='pysuperuser',
     version=pysuperuser.__version__,
     packages=['pysuperuser'],
+    install_requires=[
+        "pywin32;os_name=='nt'",
+    ],
     entry_points={
         'console_scripts': ['python-as-superuser=pysuperuser.run_python:main']
     },
